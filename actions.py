2846e6057a69864c6d526517659a9efc18fbd970
Volkan Ozer <volkian777@gmail.com>
2020-03-30 08:56:34 +0300

    actions

--- actions.py
+++ actions.py
@@ -17,21 +17,35 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_inform_restaurant"
+
+    def run(self, dispatcher, tracker, domain):
+
+        ## prod = tracker.get_slot('product')
+        price = tracker.get_slot('price')
+        name =  "Mr.Hamza" # later generate through some process
+
+        response = """Your price {} is ordered for you. It will be shipped to your address. Your confirmation number is {}""".format(
+            price, name)
+
+        dispatcher.utter_message(text=response)
+        return []
+
+
+    '''
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
+    '''
